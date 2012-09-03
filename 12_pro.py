15aed76d42517f28341328189189b9025a3dae9b
zt <zt@zt-pc.(none)>
2012-09-03 17:48:59 +0800

    为什么我写的都跑得这么慢 挫败感

--- 12_pro.py
+++ 12_pro.py
@@ -1,3 +1,16 @@
-def fac_cnt():
+def num_of_fac(i):
+    cnt=0
+    for x in range(1,i+1):
+        if i%x==0:
+            cnt+=1
+    return cnt
+i=1
+tri_num=1
+while(True):
+     if num_of_fac(tri_num)>=500:
+         break
+     i+=1
+     tri_num+=i
+     print i
+print i 
 
-tri_num=
